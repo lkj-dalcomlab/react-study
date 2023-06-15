10bdf2a2a767dac4e55361677d755c4347e348c2
lkj-dalcomlab <lkj@dalcomlab.com>
2023-06-15 15:00:51 +0900

    AttributesGrid 변경 (fixed #1)
     - 기존 텍스트 나열 -> 테이블 형태로 변경

--- src/demo/layout/management/AttributesGrid.js
+++ src/demo/layout/management/AttributesGrid.js
@@ -2,9 +2,49 @@ import {Button, Dialog, DialogContent, DialogTitle, ListItem, Typography} from "
 import PageviewIcon from "@mui/icons-material/Pageview";
 import * as React from "react";
 import {useState} from "react";
+import GridCellTooltip from "./GridCellTooltip";
+import CustomPagination from "../dashboard/process/table/mui/CustomPagination";
+import CustomNoRowsOverlay from "../dashboard/process/table/mui/CustomNoRowsOverlay";
+import {DataGrid, koKR} from "@mui/x-data-grid";
 
 export default function AttributesGrid({attributes}) {
+    console.log(attributes)
+    const rows = [];
+    attributes.map((attribute, idx)=> {
+        rows.push({
+            id:idx+1, Name:Object.keys(attribute), Value:Object.values(attribute),
+        });
+        return '';
+    });
+
+    const columns = [
+        { field: 'id', headerName: 'No', flex: 0.1 },
+        {
+            field: 'Name',
+            headerName: 'Name',
+            headerAlign: 'center',
+            align: 'center',
+            flex: 1,
+            editable: false,
+            renderCell: GridCellTooltip
+        },
+        {
+            field: 'Value',
+            headerName: 'Value',
+            headerAlign: 'center',
+            align: 'center',
+            flex: 1.2,
+            renderCell: GridCellTooltip
+        }
+    ];
+
+    const [paginationModel, setPaginationModel] = useState({
+        pageSize: 10,
+        page: 0,
+    })
+
     const [attrOpen, setAttrOpen] = useState(false);
+
     const openAttributeGrid = () => {
         setAttrOpen(true);
     };
@@ -19,22 +59,42 @@ export default function AttributesGrid({attributes}) {
                     onClick={openAttributeGrid}>
                 {attributes.length}
             </Button>
-            <Dialog open={attrOpen} onClose={closeAttributeGrid}>
+            <Dialog open={attrOpen} onClose={closeAttributeGrid}
+                    fullWidth={true} maxWidth='md'
+            >
                 <DialogTitle>
                     <div>
                         <Typography variant="h4">Attributes</Typography>
                     </div>
                 </DialogTitle>
                 <DialogContent>
-                    {
-                        attributes.map((attribute, idx) => {
-                            return (
-                                <ListItem key={idx}>
-                                    <Typography>{idx+1 + ' - ' + attribute}</Typography>
-                                </ListItem>
+                    <DataGrid
+                        sx={{width: '100%', padding: '0 5px 0 5px',
+                            ...(
+                                {'& .MuiDataGrid-columnHeader:focus, .MuiDataGrid-cell:focus': {
+                                        outline: 'none'
+                                    }}
                             )
-                        })
-                    }
+                        }}
+                        rows={rows}
+                        columns={columns}
+
+                        disableRowSelectionOnClick
+
+                        onRowSelected={(GridRowSelectedParams) => {
+                            console.log(GridRowSelectedParams);
+                        }}
+
+                        paginationModel={paginationModel}
+                        onPaginationModelChange={setPaginationModel}
+
+                        slots={{
+                            pagination: CustomPagination,
+                            noRowsOverlay: CustomNoRowsOverlay
+                        }}
+
+                        localeText={koKR.components.MuiDataGrid.defaultProps.localeText}
+                    />
                 </DialogContent>
             </Dialog>
         </>
